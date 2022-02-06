77ee7c3e54c9b123c57811c9ec61a4b3f156d5b1
junyharang <junyharang8592@gmail.com>
2022-02-06 22:03:07 +0900

    2. 대소문자 변환 - ASCII Code로 풀어보기 완료

--- src/CaseConversion.java
+++ src/CaseConversion.java
@@ -42,15 +42,32 @@ public class CaseConversion {
 
         for (char c : str.toCharArray()) {
 
-            if (Character.isLowerCase(c)) {         // 문자 변수 c의 값이 소문자 인가?  isLowerCase는 boolean Type으로 반환
+            /**
+             * ASCII Code 에서 대문자는 65 ~ 90
+             * 소문자는 97 ~ 122이다.
+             * 소문자 a에서 대문자 A의 수를 빼면 32가 되는데, 소문자에서 대문자를 빼면 그렇다라는 규칙이 생신다.
+             */
 
-                answer += String.valueOf(Character.toUpperCase(c));         // 문자 변수 c 값을 대문자로 바꿔 문자열 변수 answer에 추가해라
+            if ( c >= 65 && c <= 90) {      // 문자 변수 c의 값이 ASCII 값 65(대문자 A)보다 크고, 90(대문자 Z) 보다 작다면?
+                answer += (char) (c + 32);
+            } else if (c >= 97 && c <= 122) {   // 문자 변수 c의 값이 ASCII 값 97(소문자 a)보다 크고, 122(소문자 z) 보다 작다면?
+                answer += (char) (c - 32);
+            } else {   // 입력 값으로 띄어쓰기가 들어오면 끝나버리기 때문에 입력
+                System.out.println(c);
+                answer += (char) (c);
+            }  // if-else if-else 끝
 
-            } else if (Character.isUpperCase(c)) {  // 문자 변수 c의 값이 대문자 인가?  isLowerCase는 boolean Type으로 반환
+            /* ASCII 로 풀이 하기 위해 주석 처리 */
 
-                answer += String.valueOf(Character.toLowerCase(c));         // 문자 변수 c 값을 소문자로 바꿔 문자열 변수 answer에 추가해라
-
-            } // if-else if문 끝
+//            if (Character.isLowerCase(c)) {         // 문자 변수 c의 값이 소문자 인가?  isLowerCase는 boolean Type으로 반환
+//
+//                answer += String.valueOf(Character.toUpperCase(c));         // 문자 변수 c 값을 대문자로 바꿔 문자열 변수 answer에 추가해라
+//
+//            } else if (Character.isUpperCase(c)) {  // 문자 변수 c의 값이 대문자 인가?  isLowerCase는 boolean Type으로 반환
+//
+//                answer += String.valueOf(Character.toLowerCase(c));         // 문자 변수 c 값을 소문자로 바꿔 문자열 변수 answer에 추가해라
+//
+//            } // if-else if문 끝
         } // for (char c : str.toCharArray()) 끝
 
         return answer;
